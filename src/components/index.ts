40c171178342e8eef9c21f1e08cc81b6cc1cb9be
Thanukamax <159769064+Thanukamax@users.noreply.github.com>
2026-02-19 22:36:29 +0530

    chore: update package.json to add types and defaults for globe

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@ export * from '../lib/constants';
 export * from '../lib/types';
 export * from './backgrounds/AnimatedBackground';
 export * from './backgrounds/DashboardBackground';
-
 export * from './backgrounds/Globe';
 export * from './backgrounds/GradientBackground';
 export * from './buttons/Button';
